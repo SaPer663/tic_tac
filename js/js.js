3bc8a5bf52a89015637bd9295b7de81a3154a189
saper <cheblin@yandex.ru>
2018-08-27 15:09:25 +0500

    first commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+//let arr = [ 1, 2, 3, 4 ] ;
+const delElem = ( arr, elem ) => {
+	let len = arr.length ;
+	let rezult = 0 ;
+	for ( let i = 0 ; i < len ; i += 1) {
+		if ( arr[i] === elem ) {
+		  arr.splice( i, 1 ) ;
+		}
+	}
+	return arr ;
+} ;
+
+const arrMain = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 1, 4, 7 ],
+	[ 2, 5, 8 ], [ 3, 6, 9 ], [1, 5, 9 ], [ 3, 5, 7 ] ] ;
+let possibleCourse = [] ;  // массив возможных вариантов след. хода
+const findArr = ( arr, elem ) => {
+	let rezult = [] ;
+	let len = arr.length ;
+	for ( let i = 0 ; i < len ; i += 1 ) {
+	  let a = arr [ i ] ;
+	  for ( let j = 0 ; j < a.length ; j += 1 ) {
+			if ( a[ j ] === elem ) {
+					possibleCourse.push ( delElem ( a , elem ) ) ;
+			}
+		}
+	}
+};
+findArr ( arrMain, 2 ) ;
+console.log ( possibleCourse ) ;
