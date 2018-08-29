a1c648d1f1fc9f70cf24a2d2f154eabf9e3f860e
saper <cheblin@yandex.ru>
2018-08-29 10:22:59 +0500

    "работа с файлом js"

--- js/js.js
+++ js/js.js
@@ -14,7 +14,7 @@ const arrMain = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 1, 4, 7 ],
 	[ 2, 5, 8 ], [ 3, 6, 9 ], [1, 5, 9 ], [ 3, 5, 7 ] ] ;
 let possibleCourse = [] ;  // массив возможных вариантов след. хода
 const findArr = ( arr, elem ) => {
-	let rezult = [] ;
+//	let rezult = [] ;
 	let len = arr.length ;
 	for ( let i = 0 ; i < len ; i += 1 ) {
 	  let a = arr [ i ] ;
@@ -27,15 +27,18 @@ const findArr = ( arr, elem ) => {
 };
 
 const playerX = ( i ) => {
-	
+
 	i = 'x';
-	
+
 };
 const playerO = (i) => {
 	i = 'o';
 };
-	
+
 playerX( gamer );
 console.log(gamer);
 playerX( artInt, gamer );
 console.log( artInt );
+let gamersCourse = [];
+const arrId = [ '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
+getElementById('id')
