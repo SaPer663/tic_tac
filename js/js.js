31bbf74317a70670d3ed50ae169a91fca6256bdd
saper663_work <cheblin@yandex.ru>
2018-09-01 15:37:38 +0500

    продолжение, сбор данных из элементов по id

--- js/js.js
+++ js/js.js
@@ -32,20 +32,49 @@ const gamerCoice = ( x, o ) => { // присвоение метки игроку
   gamer = x;
   artInt = o;
 };
+
 const beforeStartGame = () => gamer !== '' ? startGame() : alert( ' Выбери чем играешь и нажми "Играть"! ') ; 
+
 const startGame = () => {
   if ( gamer === 'o' ) {
     return  crossId ( 'five5', artInt ) ;
   }
 };
-let gamersCourse = [];
-let valueId = 0;
-const arrId = [ '0', 'one1', 'two2', 'three3', 'four4', 'five5', 'six6', 'seven7', 'eight8', 'nine9' ]; //массив id
+
+//const checkOnFilling = () => 
 
 const crossId = ( id, player ) => document.getElementById( id ).textContent = player; // функ. получает 
                                                                                      //id и метку и заполняет соотв. клетку
-                                                                          
-//console.log (artInt);
-//crossId( '5', artInt);
-//let text = document.getElementById('five').textContent;
-//console.log(text);
+let gamersCourse = [];
+let valueId = 0;
+const arrId = [ '0', 'one1', 'two2', 'three3', 'four4', 'five5', 'six6', 'seven7', 'eight8', 'nine9' ]; //массив id
+//let listElemWithId = [];
+const findArrId = () => { // функ. собирает cell с id в массив
+	let arr = document.getElementsByClassName( ' cell ');
+	let listElemWithId = [];
+	for ( let i = 0; i < arr.length; i += 1 ) {
+	  if ( arr[ i ].id ) {
+	    listElemWithId.push( arr[ i ].id)
+	  }
+	  }
+	return listElemWithId;  
+};
+let a = findArrId();
+console.log( a );
+//console.log( typeof(document.getElementById('one1').textContent) );
+
+const findContentElemById = ( arr ) => {
+	let listContElemById = [];
+	let len = arr.length;
+    for ( let i = 0; i < len; i += 1 ) {
+	   // alert( document.getElementById( arr[ i ]).textContent);
+		listContElemById.push( document.getElementById( arr[ i ] ).textContent);
+		//alert( document.getElementById( arr[ i ]).textContent);
+	}
+	return listContElemById;
+};
+let b = findContentElemById(findArrId());
+console.log(b); 
+//;
+console.log( findContentElemById( findArrId ) );
+const show = () => alert( document.getElementById('one1').textContent);
